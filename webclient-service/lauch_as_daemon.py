34dad62c31d988b3a64ad29405d81bf5aa0cb547
Vincent Cagnard <vincent.cagnard@davidson.fr>
2023-07-10 23:49:32 +0200

    oup's inverted removed script

--- webclient-service/lauch_as_daemon.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Davidson <twister@davidson.fr>
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-import os, time, sys, subprocess
-
-sys.argv.pop(0)
-print(sys.argv)
-subprocess.Popen(sys.argv, close_fds=True)
