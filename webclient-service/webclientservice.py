77af4185187adcbe7b4a0d8bc63f99c505457dcf
Vincent Cagnard <vincent.cagnard@davidson.fr>
2023-07-10 22:40:09 +0200

    refactoring selection of element with protection

--- webclient-service/webclientservice.py
+++ webclient-service/webclientservice.py
@@ -36,55 +36,55 @@ class WebClientService():
                 window_size = command['target'].split('x')
                 self.driver.set_window_size(window_size[0], window_size[1])
             elif command['command'] == 'click':
-                target = command['target'].split('=')
-                if target[0] == 'id':
-                    WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.ID, target[1])))
-                    self.driver.find_element(By.ID, target[1]).click()
-                elif target[0] == 'css':
-                    WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, target[1])))
-                    self.driver.find_element(By.CSS_SELECTOR, target[1]).click()
-                elif target[0] == 'linkText':
-                    WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.LINK_TEXT, target[1])))
-                    self.driver.find_element(By.LINK_TEXT, target[1]).click()
-                else:
-                    print(f'unknow selector for {target[0]}')
-                wait_for_ajax(self.driver)
+                element = self.get_element(command['target'])
+                if element != None:
+                    element.click()
+                self.wait_for_ajax()
             elif command['command'] == 'type':
-                target = command['target'].split('=')
-                if target[0] == 'id':
-                    WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.ID, target[1])))
-                    self.driver.find_element(By.ID, target[1]).send_keys(command['value'])
-                else:
-                    print(f'unknow selector for {target[0]}')
-                wait_for_ajax(self.driver)
+                element = self.get_element(command['target'])
+                if element != None:
+                    element.send_keys(command['value'])
+                self.wait_for_ajax()
             elif command['command'] == 'sendKeys':
-                target = command['target'].split('=')
                 value = command['value'].replace('${KEY_', '').replace('}', '')
-                if target[0] == 'id':
-                    WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.ID, target[1])))
-                    self.driver.find_element(By.ID, target[1]).send_keys(eval('Keys.' + value))
-                else:
-                    print(f'unknow selector for {target[0]}')
-                wait_for_ajax(self.driver)
+                element = self.get_element(command['target'])
+                if element != None:
+                    element.send_keys(eval('Keys.' + value))
+                self.wait_for_ajax()
             else:
                 print(f'unknow command {command["command"]}')
 
+    def get_element(self, target):
+        target = target.split('=')
+        if target[0] == 'id':
+            WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.ID, target[1])))
+            return self.driver.find_element(By.ID, target[1])
+        elif target[0] == 'css':
+            WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, target[1])))
+            return self.driver.find_element(By.CSS_SELECTOR, target[1])
+        elif target[0] == 'linkText':
+            WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.LINK_TEXT, target[1])))
+            return self.driver.find_element(By.LINK_TEXT, target[1])
+        else:
+            print(f'unknow selector for {target[0]}')
+            return None
+
     def prepare(self):
         if len(self.before_test_actions) > 0:
-            wait_for_file(self.file_prepare)
+            self.wait_for_file(self.file_prepare)
             print("prepare")
             self.play_scenario(self.before_test_actions)
             os.remove(self.file_prepare)
 
     def run(self):
-        wait_for_file(self.file_run)
+        self.wait_for_file(self.file_run)
         print("run")
         self.play_scenario(self.run_test_actions)
         os.remove(self.file_run)
 
     def release(self):
         if len(self.after_test_actions) > 0:
-            wait_for_file(self.file_release)
+            self.wait_for_file(self.file_release)
             print("release")
             self.play_scenario(self.after_test_actions)
             os.remove(self.file_release)
@@ -117,26 +117,19 @@ class WebClientService():
         else:
             self.run_test_actions = actions
 
-#        print(self.scenario_url)
+    def wait_for_file(self, file):
+        while not os.path.exists(file):
+            time.sleep(1)
 
-#        print(self.before_test_actions)
-#        print('--')
-#        print(self.run_test_actions)
-#        print('--')
-#        print(self.after_test_actions)
-#        sys.exit('stop')
+    def wait_for_ajax(self):
+        wait = WebDriverWait(self.driver, 15)
+        try:
+            wait.until(lambda driver: driver.execute_script('return jQuery.active') == 0)
+            wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+        except Exception as e:
+            pass
 
-def wait_for_file(file):
-    while not os.path.exists(file):
-        time.sleep(1)
-
-def wait_for_ajax(driver):
-    wait = WebDriverWait(driver, 15)
-    try:
-        wait.until(lambda driver: driver.execute_script('return jQuery.active') == 0)
-        wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-    except Exception as e:
-        pass
+# ------------------------------------------------------------------------------ 
 
 if __name__ == '__main__':
     import sys
